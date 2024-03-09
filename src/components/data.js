69d04412aeeb793b248e738877b0417fad077087
allan <allanrey.macasaquit@gmail.com>
2024-03-08 22:21:48 -0500

    ServicesLinks

--- src/components/data.js
+++ src/components/data.js
@@ -7,6 +7,45 @@ export const pageLinks = [
 
 export const socialinks = [
 	{ id: 1, href: '#home', icon: 'fab fa-facebook' },
-	{ id: 3, href: '#about', icon: 'fab fa-twitter' },
-	{ id: 2, href: '#services', icon: 'fab fa-squarespace' },
+	{ id: 2, href: '#about', icon: 'fab fa-twitter' },
+	{ id: 3, href: '#services', icon: 'fab fa-squarespace' },
+];
+
+export const serviceslinks = [
+	{
+		id: 1,
+		title: 'saving money',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, official',
+		icon: 'fas fa-wallet fa-fw',
+	},
+	{
+		id: 2,
+		title: 'endless hiking',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, official',
+		icon: 'fas fa-mountain fa-fw',
+	},
+	{
+		id: 3,
+		title: 'amazing comfort',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, official',
+		icon: 'fas fa-tree fa-fw',
+	},
+	{
+		id: 4,
+		title: 'biking thrills',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, official',
+		icon: 'fas fa-bicycle fa-fw',
+	},
+	{
+		id: 5,
+		title: 'adventure trip',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, official',
+		icon: 'fas fa-route fa-fw',
+	},
+	{
+		id: 6,
+		title: 'restroom',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, official',
+		icon: 'fas fa-restroom fa-fw',
+	},
 ];
