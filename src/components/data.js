72ca9fbfac05d9172f16b08d1c78c33d5ba667d8
allan <allanrey.macasaquit@gmail.com>
2024-03-08 21:06:36 -0500

    PageLinks

---
+++ src/components/data.js
@@ -0,0 +1,6 @@
+export const pageLinks = [
+	{ id: 1, href: '#home', text: 'home' },
+	{ id: 3, href: '#about', text: 'about' },
+	{ id: 2, href: '#services', text: 'services' },
+	{ id: 4, href: '#tours', text: 'tours' },
+];
