57de3613ee4377ea9788e2491cdaecd0d036296b
allan <allanrey.macasaquit@gmail.com>
2024-03-08 23:07:23 -0500

    Footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,48 +1,31 @@
+import { pageLinks, socialinks } from './data';
+
 const Footer = () => {
+	let currentYear = new Date().getFullYear();
 	return (
 		<footer className='section footer'>
 			<ul className='footer-links'>
-				<li>
-					<a href='#home' className='footer-link'>
-						home
-					</a>
-				</li>
-				<li>
-					<a href='#about' className='footer-link'>
-						about
-					</a>
-				</li>
-				<li>
-					<a href='#services' className='footer-link'>
-						services
-					</a>
-				</li>
-				<li>
-					<a href='#featured' className='footer-link'>
-						featured
-					</a>
-				</li>
+				{pageLinks.map(({ id, href, text }) => (
+					<li key={id}>
+						<a href={href} className='footer-link'>
+							{text}
+						</a>
+					</li>
+				))}
 			</ul>
+
 			<ul className='footer-icons'>
-				<li>
-					<a href='https://www.twitter.com' target='_' className='footer-icon'>
-						<i className='fab fa-facebook'></i>
-					</a>
-				</li>
-				<li>
-					<a href='https://www.twitter.com' target='_' className='footer-icon'>
-						<i className='fab fa-twitter'></i>
-					</a>
-				</li>
-				<li>
-					<a href='https://www.twitter.com' target='_' className='footer-icon'>
-						<i className='fab fa-squarespace'></i>
-					</a>
-				</li>
+				{socialinks.map(({ id, href, icon }) => (
+					<li key={id}>
+						<a href={href} target='_' className='footer-icon'>
+							<i className={icon}></i>
+						</a>
+					</li>
+				))}
 			</ul>
 			<p className='copyright'>
 				copyright &copy; Backroads travel tours company
-				<span id='date'></span> all rights reserved
+				<span id='date'>{currentYear}.</span> all rights reserved
 			</p>
 		</footer>
 	);
